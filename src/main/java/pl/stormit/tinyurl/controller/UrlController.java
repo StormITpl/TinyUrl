0069afeed9494ec0a85aef84923692470593314e
RobertoJavaDev <robertojavadev@gmail.com>
2022-10-12 08:45:12 +0200

    implemented createShortUrl method in Controller

--- src/main/java/pl/stormit/tinyurl/controller/UrlController.java
+++ src/main/java/pl/stormit/tinyurl/controller/UrlController.java
@@ -1,26 +1,30 @@
 package pl.stormit.tinyurl.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.stormit.tinyurl.domain.model.Url;
+import pl.stormit.tinyurl.dto.UrlDto;
 import pl.stormit.tinyurl.service.UrlService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/urls")
+@AllArgsConstructor
 public class UrlController {
 
+    @Autowired
     private final UrlService urlService;
 
-    public UrlController(UrlService urlService) {
-        this.urlService = urlService;
+    @PostMapping
+    public ResponseEntity<Object> createShortUrl(@RequestBody UrlDto urlDto) {
+        return ResponseEntity.ok(urlService.generateShortUrl(urlDto));
     }
 
     @GetMapping
-    public List<Url> getUrl(){
+    public List<Url> getUrl() {
         return urlService.getUrl();
     }
-
 }
