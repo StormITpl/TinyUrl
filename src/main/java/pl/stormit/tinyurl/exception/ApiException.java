abdcb76f2fa7f410e642cdad4572e3d127fad192
RobertoJavaDev <robertojavadev@gmail.com>
2022-10-16 13:32:52 +0200

    implemented ApiException method

--- src/main/java/pl/stormit/tinyurl/exception/ApiException.java
+++ src/main/java/pl/stormit/tinyurl/exception/ApiException.java
@@ -1,4 +1,8 @@
 package pl.stormit.tinyurl.exception;
 
-public class ApiException {
+public class ApiException extends RuntimeException {
+
+    public ApiException(String errorMessage) {
+        super(errorMessage);
+    }
 }
