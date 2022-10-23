f0c6a3a5e9c6b4d2b180c41b1d6e70760f1da722
RobertoJavaDev <robertojavadev@gmail.com>
2022-10-23 14:01:15 +0200

    changed type of exception in ExceptionHandler

--- src/main/java/pl/stormit/tinyurl/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/stormit/tinyurl/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.stormit.tinyurl.exception;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,10 +12,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ApiException.class)
-    public final ResponseEntity<Object> handleMethodArgumentNotValidException(ApiException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public final ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, Object> map = new HashMap<>();
         map.put("error", ex.getMessage());
         return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
--- src/test/java/pl/stormit/tinyurl/controller/UrlControllerTest.java
+++ src/test/java/pl/stormit/tinyurl/controller/UrlControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.stormit.tinyurl.domain.model.Url;
 import pl.stormit.tinyurl.dto.UrlDto;
 import pl.stormit.tinyurl.service.UrlService;
