b2dc8ab9f6132f51c895226182b7ef96070f6db2
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-15 08:46:51 +0100

    added lombok annotations and constructor

--- src/main/java/pl/stormit/tinyurl/domain/model/UrlAnalytics.java
+++ src/main/java/pl/stormit/tinyurl/domain/model/UrlAnalytics.java
@@ -1,11 +1,17 @@
 package pl.stormit.tinyurl.domain.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 @Entity
 @Table (name = "url_analytics")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class UrlAnalytics {
 
     @Id
@@ -22,4 +28,10 @@ public class UrlAnalytics {
 
     @OneToOne
     private Url url;
+
+    UrlAnalytics(final Long totalClicks, final String localization, final Instant clickDate) {
+        this.totalClicks = totalClicks;
+        this.localization = localization;
+        this.clickDate = Instant.now();
+    }
 }
