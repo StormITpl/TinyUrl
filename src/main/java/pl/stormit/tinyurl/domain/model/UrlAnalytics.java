6739fbc79b21abe9c675b53937d195e5915a53b9
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-15 08:32:33 +0100

    implemented UrlAnalytics entity

--- src/main/java/pl/stormit/tinyurl/domain/model/UrlAnalytics.java
+++ src/main/java/pl/stormit/tinyurl/domain/model/UrlAnalytics.java
@@ -1,4 +1,25 @@
 package pl.stormit.tinyurl.domain.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Entity
+@Table (name = "url_analytics")
 public class UrlAnalytics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private Long totalClicks;
+
+    private String localization;
+
+    @NotBlank
+    private Instant clickDate;
+
+    @OneToOne
+    private Url url;
 }
