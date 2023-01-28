534f9ede029624e652f7fea07261adddbae1ed38
RobertoJavaDev <robertojavadev@gmail.com>
2023-01-28 07:36:04 +0100

    feat: Added find all by UrlId method to repository interface

--- src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
+++ src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.stormit.tinyurl.domain.model.UrlAnalytics;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UrlAnalyticsRepository extends JpaRepository<UrlAnalytics, UUID> {
+    List<UrlAnalytics> findAllByUrlId(UUID id);
 }
