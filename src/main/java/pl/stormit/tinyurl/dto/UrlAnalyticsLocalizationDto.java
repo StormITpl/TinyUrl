bd47c872eae75edc66a834a1fd4d31f61c3180ed
RobertoJavaDev <robertojavadev@gmail.com>
2023-04-19 08:43:39 +0200

    feat: Added new localization class

---
+++ src/main/java/pl/stormit/tinyurl/dto/UrlAnalyticsLocalizationDto.java
@@ -0,0 +1,15 @@
+package pl.stormit.tinyurl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UrlAnalyticsLocalizationDto {
+
+    private String countryLocalization;
+
+    private String isoCode;
+
+    private String cityLocalization;
+}
