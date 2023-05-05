72736acce10adbe3afa6d1190e8d79c4ddb15025
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-05 08:39:34 +0200

    feat: Added find method to repository with custom query

--- src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
+++ src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.stormit.tinyurl.domain.model.UrlAnalytics;
+import pl.stormit.tinyurl.dto.UrlAnalyticsDto;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,4 +16,12 @@ public interface UrlAnalyticsRepository extends JpaRepository<UrlAnalytics, UUID
     Long findMaxClickOnShortUrlByUrlId(UUID id);
 
     List<UrlAnalytics> findAllByUrlId(UUID id);
+
+    @Query("SELECT u.shortUrl, a.totalClicks as totalClicks " +
+            "FROM UrlAnalytics a " +
+            "JOIN a.url u " +
+            "GROUP BY u.shortUrl " +
+            "ORDER BY totalClicks DESC " +
+            "LIMIT 5")
+    List<UrlAnalyticsDto[]> findMostPopularUrls();
 }
