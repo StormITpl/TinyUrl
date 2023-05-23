d73e871cb87708a8fa3e1a1bde84366319799e1c
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-23 09:22:40 +0200

    feat: Implemented query to urlRepository

--- src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
+++ src/main/java/pl/stormit/tinyurl/domain/repository/UrlAnalyticsRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.stormit.tinyurl.domain.model.UrlAnalytics;
-import pl.stormit.tinyurl.dto.UrlAnalyticsDto;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,12 +15,4 @@ public interface UrlAnalyticsRepository extends JpaRepository<UrlAnalytics, UUID
     Long findMaxClickOnShortUrlByUrlId(UUID id);
 
     List<UrlAnalytics> findAllByUrlId(UUID id);
-
-    @Query("SELECT u.shortUrl, a.totalClicks as totalClicks " +
-            "FROM UrlAnalytics a " +
-            "JOIN a.url u " +
-            "GROUP BY u.shortUrl " +
-            "ORDER BY totalClicks DESC " +
-            "LIMIT 5")
-    List<UrlAnalyticsDto[]> findMostPopularUrls();
 }
