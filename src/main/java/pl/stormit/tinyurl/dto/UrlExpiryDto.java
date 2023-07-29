c3e29f1b978a78036e3db09a3f6d2f50da07ae2c
RobertoJavaDev <robertojavadev@gmail.com>
2023-07-29 14:03:47 +0200

    feat: Refactored dto's

--- src/main/java/pl/stormit/tinyurl/dto/UrlExpiryDto.java
+++ src/main/java/pl/stormit/tinyurl/dto/UrlExpiryDto.java
@@ -1,12 +1,16 @@
 package pl.stormit.tinyurl.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlExpiryDto {
 
     @NotBlank
