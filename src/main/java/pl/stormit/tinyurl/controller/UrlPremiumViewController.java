270721762a2c7b9c15da79a3fe9babb4a6447621
RobertoJavaDev <robertojavadev@gmail.com>
2023-08-06 19:03:38 +0200

    feat: Modified premium view controller

--- src/main/java/pl/stormit/tinyurl/controller/UrlPremiumViewController.java
+++ src/main/java/pl/stormit/tinyurl/controller/UrlPremiumViewController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,34 +22,31 @@ public class UrlPremiumViewController {
 
     private final UrlService urlService;
 
-
     @GetMapping
     public String showPremiumForm(Model model) {
         UrlDto urlDto = new UrlDto();
         model.addAttribute("urlDto", urlDto);
         model.addAttribute("allUrls", urlService.getUrls());
-        return "indexPremium";
-    }
-
-    @GetMapping("add")
-    public String addView(Model model){
-        UrlDto urlDto = new UrlDto();
-        model.addAttribute("urlDto", urlDto);
-
 
         return "indexPremium";
     }
 
-    @PostMapping
-    public String generateShortUrl(UrlDto urlDto){
-        urlService.generateShortUrl(urlDto);
+    @PostMapping("generate")
+    public String generateShortUrl(Model model, UrlDto urlDto) {
+        UrlDto generatedUrlDto = urlService.generateShortUrl(urlDto);
+        urlDto.setShortUrl(generatedUrlDto.getShortUrl());
+        model.addAttribute("allUrls", urlService.getUrls());
+        model.addAttribute("activeForm", "form1");
 
         return "indexPremium";
     }
 
     @PostMapping("create")
-    public String createShortUrl(UrlDto urlDto){
-        urlService.createShortUrl(urlDto);
+    public String createShortUrl(Model model, UrlDto urlDto) {
+        UrlDto createdUrlDto = urlService.createShortUrl(urlDto);
+        urlDto.setShortUrl(createdUrlDto.getShortUrl());
+        model.addAttribute("allUrls", urlService.getUrls());
+        model.addAttribute("activeForm", "form2");
 
         return "indexPremium";
     }
